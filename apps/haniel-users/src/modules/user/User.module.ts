7c678c4ac506321ea31555e77173b440fbcd06eb
mauricio-bs <mauricio.3g2014@gmail.com>
2025-08-28 15:10:18 -0300

    fix(haniel): add missing controller

--- apps/haniel-users/src/modules/user/User.module.ts
+++ apps/haniel-users/src/modules/user/User.module.ts
@@ -1,12 +1,14 @@
 import { PrismaService } from '@common/database/prisma/prisma.service';
 import { Module } from '@nestjs/common';
-import { IUserService } from './domain/service/IUser.service';
-import { UserService } from './service/user.service';
+
 import { IUserRepository } from '@repository/IUser.repository';
 import { UserRepository } from '@repository/implements/User.repository';
+import { IUserService } from './domain/service/IUser.service';
+import { UserService } from './service/user.service';
+import { UserController } from './infra/amqp/controller/user.controller';
 
 @Module({
-  controllers: [],
+  controllers: [UserController],
   providers: [
     PrismaService,
     { provide: IUserService, useClass: UserService },
