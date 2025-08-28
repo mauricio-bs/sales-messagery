152511e7f0778116cec614acbb6c7bb239100cec
mauricio-bs <mauricio.3g2014@gmail.com>
2025-08-28 15:08:01 -0300

    fix(haniel): add missing global providers

--- apps/haniel-users/src/app.module.ts
+++ apps/haniel-users/src/app.module.ts
@@ -1,9 +1,17 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
+import { RmqCacheInterceptor } from '@interceptor/http-cache.interceptor';
 import { DatabaseModule } from '@common/database/database.module';
 import { ConfigurationsModule } from '@config/configurations.module';
 import { IndexModule } from '@modules/Index.module';
-import { Module } from '@nestjs/common';
 
 @Module({
   imports: [IndexModule, ConfigurationsModule, DatabaseModule],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_INTERCEPTOR, useClass: RmqCacheInterceptor },
+  ],
 })
 export class AppModule {}
